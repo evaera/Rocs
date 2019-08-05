1600dc2e168739ae6acfd3e8810b1163cc1043d7
Eryn Lynn <eryn@eryn.io>
2019-08-05 04:00:34 -0400

    Delete old files

--- legacy/OnlyOneChest/Client.lua
+++
@@ -1,1 +0,0 @@
-local Rocs = require(path.to.rocs.instance)
--- old/Query/QueryFilter.lua
+++
@@ -1,6 +0,0 @@
-local QueryFilter = {}
-QueryFilter.__index = QueryFilter
-
-function QueryFilter.new(options)
-	return setmetatable(options, QueryFilter)
-end
